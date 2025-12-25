0e3fdbc7215c05e5c47c59f368f89ab7a9f386e4
dsqshansen <dsqshansen@gmail.com>
2025-12-25 13:54:42 -0800

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,1 +1,16 @@
-export const products = [];
+export const products = [
+  {
+    id: "a",
+    name: "产品 A",
+    description: "这是一个示例产品描述",
+    image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+    region: "north"
+  },
+  {
+    id: "b",
+    name: "产品 B",
+    description: "这是另一个示例产品描述",
+    image: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+    region: "south"
+  }
+];
