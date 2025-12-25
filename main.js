3bbde7f364cafb9282d10fb4ae6ea2db6265899e
dsqshansen <dsqshansen@gmail.com>
2025-12-25 15:31:32 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("main.js loaded");
+import { products } from "./data/products.js";
+
+const container = document.getElementById("products");
+
+function renderProducts(list) {
+  container.innerHTML = "";
+  list.forEach((product) => {
+    const div = document.createElement("div");
+    div.className = "product";
+    div.innerHTML = `
+      <img src="${product.image}">
+      <h3>${product.name}</h3>
+      <p>${product.description}</p>
+    `;
+    container.appendChild(div);
+  });
+}
+
+renderProducts(products);
