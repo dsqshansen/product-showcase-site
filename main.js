f192a0f6b765e1a690f0933d887c8a38ff7b4fcd
dsqshansen <dsqshansen@gmail.com>
2025-12-25 18:49:03 -0800

    Update main.js

--- main.js
+++ main.js
@@ -19,22 +19,7 @@ function renderProducts(list) {
 renderProducts(products);
 
 
-// 按钮筛选
-document.querySelectorAll('.filters button').forEach(button => {
-  button.addEventListener('click', () => {
 
-    // 清空所有按钮的高亮
-    document.querySelectorAll('.filters button')
-      .forEach(b => b.classList.remove('active'));
-
-    // 当前点击的按钮变黑
-    button.classList.add('active');
-
-    // 执行原有筛选
-    const region = button.dataset.region;
-    renderProducts(region);
-  });
-});
 
 const defaultBtn = document.querySelector('.filters button[data-region="all"]');
 defaultBtn.classList.add("active");
