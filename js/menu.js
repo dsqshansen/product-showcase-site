335ad3fb461d104df37dc66fc0de9bbf511a3f24
dsqshansen <dsqshansen@gmail.com>
2025-12-29 16:01:08 -0800

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const menuToggle = document.getElementById("menuToggle");
+const menuClose = document.getElementById("menuClose");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.add("open");
+});
+
+menuClose.addEventListener("click", () => {
+  mobileMenu.classList.remove("open");
+});
